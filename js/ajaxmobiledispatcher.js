cb89728dd86345ed3713c79fe10811795efdc479
Sébastien Leduc <s.leduc@magnetic.coop>
2019-07-08 22:44:30 +0200

    Modifs

---
+++ js/ajaxmobiledispatcher.js
@@ -0,0 +1,29 @@
+function getBooking() {
+        
+    $('.loader2').show();    
+    var L = $("#sysLanguageUid").val();
+    var url = "/?type=476&tx_cdispofrontend_fcdispofrontend[controller]=Mobile&tx_cdispofrontend_fcdispofrontend[action]=getBooking&tx_cdispofrontend_fcdispofrontend[uid]=1&L="+L
+    $.ajax({
+          type: 'POST',
+          url:url,
+          dataType: "json",
+    
+          success: function(result) {
+                
+                if (result.result) {
+                    console.log('BAP');
+                    $('.main-slider').html(result.slide);
+                    console.log('BAP2');
+                    initSlickCarousel();
+                    console.log('BAP3');
+                } else {
+                    
+                }
+                $('.loader2').hide();
+          },  
+          error: function(error) {
+            console.log('getBooking error');
+            $('.loader2').hide();
+          }   
+    }); 
+}
