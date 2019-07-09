3b25e4cbf0f0334b123f56c407c58a983e7488bc
Leduc Sébastien <s.leduc@magnetic.coop>
2019-07-09 14:45:25 +0200

    Modifs

--- js/ajaxmobiledispatcher.js
+++ js/ajaxmobiledispatcher.js
@@ -3,10 +3,37 @@ function getBooking(cookievalue) {
     $('.loader2').show();    
     var L = 0;
     var url = "http://cdispo_preprod.moonlikestudio.com/?type=476&tx_cdispofrontend_fcdispofrontend[controller]=Mobile&tx_cdispofrontend_fcdispofrontend[action]=getBooking&tx_cdispofrontend_fcdispofrontend[uid]=1&L="+L
+    const options = {
+        method: 'post',
+        headers: { Cookie: 'fe_typo_user='+cookievalue }
+    };
+       
+      cordova.plugin.http.sendRequest(url, options, function(response) {
+        // prints 200
+        console.log(response.status);
+      }, function(response) {
+        // prints 403
+        console.log(response.status);
+       
+        //prints Permission denied
+        console.log(response.error);
+        
+        if (response.result) {
+            console.log('BAP');
+            $('.main-slider').html(response.slide);
+            console.log('BAP2');
+            initSlickCarousel();
+            console.log('BAP3');
+        } else {
+            
+        }
+        $('.loader2').hide();
+                
+      });
+    /*
     $.ajax({
             type: 'POST',
             url:url,
-            data: {fe_typo_user:cookievalue},
             dataType: "json",
             crossDomain: true,
           
@@ -35,5 +62,6 @@ function getBooking(cookievalue) {
             }
     
          
-    }); 
+    });
+    */
 }
