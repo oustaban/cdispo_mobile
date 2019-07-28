59625ad07415541064c4c1b58dbc7c0ba53c59ba
Leduc Sébastien <s.leduc@magnetic.coop>
2019-07-28 20:41:18 +0200

    Modifs

--- js/mobile.js
+++ js/mobile.js
@@ -104,9 +104,10 @@ function initMobileConnect() {
 					window.localStorage.setItem("language", cdispo_language);
 					window.localStorage.setItem("lang", array_language[cdispo_language]);
 					
-					var url = window.location.href;
-					url = url.substring(0, url.lastIndexOf("/") + 1);
-					cordova.InAppBrowser.open(url+'mesreservations.html', '_self');
+					//var url = window.location.href;
+					//url = url.substring(0, url.lastIndexOf("/") + 1);
+					//cordova.InAppBrowser.open(url+'mesreservations.html', '_self');
+					cordova.InAppBrowser.open('mesreservations.html', '_self');
 							
 					/*
 					cookieMaster.setCookieValue('http://cdispo', cookieName, agregatecookie	,
