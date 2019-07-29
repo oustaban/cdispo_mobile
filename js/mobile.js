c7b6d22114521b7c74a45f77652bd14668ad4322
Leduc Sébastien <s.leduc@magnetic.coop>
2019-07-29 10:52:17 +0200

    Modifs

--- js/mobile.js
+++ js/mobile.js
@@ -81,11 +81,9 @@ function initMobileConnect() {
 		var array_language = ['fr','en'];
 		console.log("storage:"+data);
 		
-		var wwwroot = cordova.file.applicationStorageDirectory.replace( 'file://', '' );
-		console.log('wwwroot:'+wwwroot);
-		startServer(wwwroot);
 		
-		/*
+		
+		
 		var L = 0;
 		var url = "http://cdispo_preprod.moonlikestudio.com/?type=476&tx_cdispofrontend_fcdispofrontend[controller]=Mobile&tx_cdispofrontend_fcdispofrontend[action]=dispatcher&tx_cdispofrontend_fcdispofrontend[uid]=1&L="+L
 		$.ajax({
@@ -134,7 +132,7 @@ function initMobileConnect() {
 			  }
 			  
 		});
-		*/
+		
 	
 	});
 }
@@ -198,6 +196,76 @@ function getTranslation() {
 	});
 }
 
+
+
+function updateStatus() {
+	console.log("document.location.href: " + document.location.href);
+	if( httpd ) {
+	  /* use this function to get status of httpd
+	  * if server is up, it will return http://<server's ip>:port/
+	  * if server is down, it will return empty string ""
+	  */
+		httpd.getURL(function(url){
+			if(url.length > 0) {
+				console.log("server is up: <a href='" + url + "' target='_blank'>" + url + "</a>");
+			} else {
+				console.log("server is down.");
+			}
+		});
+		// call this function to retrieve the local path of the www root dir
+		httpd.getLocalPath(function(path){
+			console.log("<br/>localPath: " + path);
+		});
+	} else {
+		alert('CorHttpd plugin not available/ready.');
+	}
+}
+
+function startServer() {
+	if ( httpd ) {
+		// before start, check whether its up or not
+		httpd.getURL(function(url){
+			if(url.length > 0) {
+				console.log("server is up: <a href='" + url + "' target='_blank'>" + url + "</a>");
+			} else {
+				/* wwwroot is the root dir of web server, it can be absolute or relative path
+				* if a relative path is given, it will be relative to cordova assets/www/ in APK.
+				* "", by default, it will point to cordova assets/www/, it's good to use 'htdocs' for 'www/htdocs'
+				* if a absolute path is given, it will access file system.
+				* "/", set the root dir as the www root, it maybe a security issue, but very powerful to browse all dir
+				*/
+				httpd.startServer({
+					'port' : 49000,
+					'localhost_only' : true
+				}, function( url ){
+				  // if server is up, it will return the url of http://<server ip>:port/
+				  // the ip is the active network connection
+				  // if no wifi or no cell, "127.0.0.1" will be returned.
+					console.log("server is started: <a href='" + url + "' target='_blank'>" + url + "</a>");
+					cordova.InAppBrowser.open(url+'index.html', '_self');
+				}, function( error ){
+					console.log('failed to start server: ' + error);
+				});
+			}
+			
+		});
+	} else {
+		alert('CorHttpd plugin not available/ready.');
+	}
+}
+
+function stopServer() {
+	if ( httpd ) {
+		// call this API to stop web server
+		httpd.stopServer(function(){
+			console.log('server is stopped.');
+		},function( error ){
+			console.log('failed to stop server' + error);
+		});
+	} else {
+		alert('CorHttpd plugin not available/ready.');
+	}
+}
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
