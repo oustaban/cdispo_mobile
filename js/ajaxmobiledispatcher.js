2f10462c020c83b298e06b8344a0bfc71e23f193
Leduc Sébastien <s.leduc@magnetic.coop>
2019-09-29 14:35:12 +0200

    Modifs

--- js/ajaxmobiledispatcher.js
+++ js/ajaxmobiledispatcher.js
@@ -3560,7 +3560,7 @@ function getScan(fe_typo_user,ressourceId,category,action) {
                             initSlickCarousel();
                             $('#backtosharelink').html(result.linktitle);
                             $('#backtosharelink').click(function() {
-                                getBookingToGo(''+fe_typo_user+'',ressourceId,category);
+                                getBookingToGo(''+fe_typo_user+'',ressourceId,category,'refresh');
                             });
                         } else {
                             $('#backtoshare').hide();
@@ -3652,6 +3652,103 @@ function unlockBooking(fe_typo_user,bookingId) {
     }); 
 }
 
+
+
+function getBookingToGo(fe_typo_user,ressourceId,category,action) {
+    var L = window.localStorage.getItem("language");
+    var lang = window.localStorage.getItem("lang");
+    $('.loader2').show();
+    var domain = window.localStorage.getItem("domain");
+    var url = "http://"+domain+"/?type=476&tx_cdispofrontend_fcdispofrontend[controller]=Mobile&tx_cdispofrontend_fcdispofrontend[action]=dispatcher&tx_cdispofrontend_fcdispofrontend[uid]=1&L="+L;
+    $.ajax({
+          type: 'GET',
+          url:url,
+          dataType: "jsonp",
+          jsonp: 'callback',
+          jsonpCallback: 'cdispoToken',
+          data: {action:"getBookingToGo",fe_typo_user:fe_typo_user,ressourceId:ressourceId,category:category},
+          
+          success: function(result) {
+                
+                if (result.deconnexion) {
+                    
+                    initPopin();
+                    $('.prewiewsharing_header').hide();
+                    $('.previewsharing_content').html('<p></p><p>'+result.deconnexion+'</p>');
+                    
+                    var url = window.location.href;
+					url = url.substring(0, url.lastIndexOf("/") + 1);
+                    window.localStorage.clear();
+                    $('#btn_close').attr('onclick','cordova.InAppBrowser.open(\''+url+'index.html\', \'_self\')');
+                    
+                    $('.main-slider').hide();$('.nav-holder').hide();
+                    $('.modification-block').hide();
+                    $('.info-block').show();
+                
+                }
+                
+                if (result.result) {
+                    console.log('success getBookingToConfirm 1 '+action);
+                    if (action == "refresh") {
+                         console.log('countslide:'+countslide);
+                         //window.clearAllIntervals();
+                         
+                         for (var i = 0; i < 1000; i++) {
+                            var varInterval = "x"+i;
+                            //window.clearInterval(varInterval);
+                            window.clearInterval(i);
+                         }
+                         
+                         $("div.slick-slide").each(function() {
+                            var i = $(this).attr("data-slick-index");
+                            $('.main-slider').slick('slickRemove',i);
+                         });
+                         
+                         
+                         $('.main-slider').show();$('.nav-holder').show();
+                         $('.info-block').hide();
+                         if (countslide > 0) 
+                            $('.main-slider').slick('unslick');
+                    }
+                    
+                    countslide = result.countslide;
+                    //console.log(countslide);
+                    //console.log(result.slide);
+                    if (countslide > 0) {
+                        $('.main-slider').html(result.slide);
+                        initSlickCarousel();
+                        $('#backtosharelink').html(result.linktitle);
+                        $('#backtosharelink').click(function() {
+                            getDispo(''+fe_typo_user+'',ressourceId,category,'refresh');
+                        });
+                    } else {
+                        $('#backtosharelink').html(result.linktitle);
+                        $('#backtosharelink').click(function() {
+                            getDispo(''+fe_typo_user+'',ressourceId,category,'refresh');
+                        });
+                        $('.prewiewsharing_header').hide();
+                        $('.previewsharing_content').html(result.content);
+                        $('#btn_close').attr('onclick','getBookingToConfirm(\''+fe_typo_user+'\',\'refresh\')');
+                        $('.main-slider').hide();$('.nav-holder').hide();
+                        $('.info-block').show();    
+                    }
+                    
+                } else {
+                    
+                }
+                
+                console.log('success getBookingToConfirm');
+                
+                
+                $('.loader2').hide();
+          },  
+          error: function(error) {
+            console.log('nok getBookingToConfirm');
+            console.log(error);
+            $('.loader2').hide();
+          }   
+    });
+}
 
 function loadSharing(fe_typo_user,indexSlide) {
     var url = window.location.href;
