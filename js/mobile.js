a0b470f8d6dca0948dfb448e01146a39e2388394
Leduc Sébastien <s.leduc@magnetic.coop>
2019-10-07 23:38:20 +0200

    Modifs

--- js/mobile.js
+++ js/mobile.js
@@ -309,9 +309,8 @@ function initInvites() {
 		});
 	
 		$('#invites').autocomplete({
-			serviceUrl: 'http://'+domain+'/?type=476&tx_cdispofrontend_fcdispofrontend[controller]=Ajax&tx_cdispofrontend_fcdispofrontend[action]=getUsersActifs&tx_cdispofrontend_fcdispofrontend[uid]=1&L=0',
-			onSelect: function (suggestion) {
-				console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
+			onSearchStart: function() {
+				console.log('YEP');	
 			}
 		});
 		
