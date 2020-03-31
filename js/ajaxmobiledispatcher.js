1157b4a7415e2077f7147e8e3d3aa543ee87afbe
Sébastien Leduc <s.leduc@magnetic.coop>
2020-03-31 12:11:31 +0200

    Modifs

--- js/ajaxmobiledispatcher.js
+++ js/ajaxmobiledispatcher.js
@@ -512,19 +512,21 @@ function getVisibilityInfo(fe_typo_user,event_id,indexSlide) {
                     $('.prewiewsharing_title').html(trad['guests']);
                     $('.previewsharing_content').html(result.content);
                     
-                    $('#btn_close').text(trad['btn_close']);
-                    $('#btn_close').attr('onclick','$(\'.modification-block2\').hide();$(\'.modification-block\').hide();$(\'.message-block\').hide();$(\'.info-block\').hide();$(\'.main-slider\').show();$(\'.nav-holder\').show();');
-                    $('#btn_cancel').text('');
-                        
-                    $('#btn_close').text(trad['sendmessagesharing']);
-                    $('.previewmessage_mandatories').html(trad['sendmessagesharingmessage']+result.sharingId+'.<br/>'+trad['sendmessagesharingmessage2']);
-                    $('#message').val('');
-                    $('#btn_close').attr('onclick','$(\'.info-block\').hide();$(\'.main-slider\').hide();$(\'.nav-holder\').hide();$(\'#backtoshare\').hide();$(\'.modification_block\').hide();$(\'.message-block\').show()');
-                    $('#btn_send').attr('onclick','sendMessageSharing('+result.sharingId+',$(\'#message\').val(),\''+fe_typo_user+'\','+indexSlide+')');
-                    $('#btn_back2').attr('onclick','$(\'.info-block\').show();$(\'.main-slider\').hide();$(\'.nav-holder\').hide();$(\'#backtoshare\').hide();$(\'.modification_block\').hide();$(\'.message-block\').hide()');
-                    $('#btn_cancel').text(trad['btn_back']);
-                    $('#btn_cancel').attr('onclick','$(\'.modification-block2\').hide();$(\'.info-block\').hide();$(\'.main-slider\').show();$(\'.nav-holder\').show();$(\'#backtoshare\').hide();$(\'.modification_block\').hide()');
+                    if (result.statut != 0) {
+                        $('#btn_close').text(trad['btn_close']);
+                        $('#btn_close').attr('onclick','$(\'.modification-block2\').hide();$(\'.modification-block\').hide();$(\'.message-block\').hide();$(\'.info-block\').hide();$(\'.main-slider\').show();$(\'.nav-holder\').show();');
+                        $('#btn_cancel').text('');
+                    } else {
                     
+                        $('#btn_close').text(trad['sendmessagesharing']);
+                        $('.previewmessage_mandatories').html(trad['sendmessagesharingmessage']+result.sharingId+'.<br/>'+trad['sendmessagesharingmessage2']);
+                        $('#message').val('');
+                        $('#btn_close').attr('onclick','$(\'.info-block\').hide();$(\'.main-slider\').hide();$(\'.nav-holder\').hide();$(\'#backtoshare\').hide();$(\'.modification_block\').hide();$(\'.message-block\').show()');
+                        $('#btn_send').attr('onclick','sendMessageSharing('+result.sharingId+',$(\'#message\').val(),\''+fe_typo_user+'\','+indexSlide+')');
+                        $('#btn_back2').attr('onclick','$(\'.info-block\').show();$(\'.main-slider\').hide();$(\'.nav-holder\').hide();$(\'#backtoshare\').hide();$(\'.modification_block\').hide();$(\'.message-block\').hide()');
+                        $('#btn_cancel').text(trad['btn_back']);
+                        $('#btn_cancel').attr('onclick','$(\'.modification-block2\').hide();$(\'.info-block\').hide();$(\'.main-slider\').show();$(\'.nav-holder\').show();$(\'#backtoshare\').hide();$(\'.modification_block\').hide()');
+                    }
                     
                     
                     $('.main-slider').hide();
